ff3c83c947268c8fb12389d0e825e1847e602bcc
nhtmai <mainhatng@gmail.com>
2025-03-02 13:58:49 -0500

    add GET concerts request

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+import os
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+import requests
 
 @api_view(["GET"])
 def home(request):
     return Response({"message": "Django Backend is Running!"})
+
+@api_view(["GET"])
+def concerts(request):
+    # getting suggested concerts
+    request_params = {
+        'apikey': os.environ["TICKETMASTER_KEY"],
+        'latlong': "43.653225,-79.383186",
+        'radius': '20',
+        'unit': 'km',
+        'classificationName': 'Music',
+        'includeTest': 'no'
+    }
+    response = requests.get(
+        f'{os.environ["TICKETMASTER_URL_BASE"]}/events',
+        params=request_params
+    )
+    events = response.json()['_embedded']
+    return JsonResponse(events)
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from api.views import home
+from api.views import home, concerts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('admin/', admin.site.urls),
     path('api/', home),
+    path('concerts/', concerts)
 ]
