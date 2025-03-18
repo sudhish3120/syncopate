ec5694834ca825ca6e880e86fe3c9c2439abe79a
Sudhish Meenakshisundaram <sudhish.3120@gmail.com>
2025-03-18 17:23:20 -0400

    fixed migrations (again)

--- backend/api/migrations/0003_remove_concert_artist_remove_concert_venue_and_more.py
+++ backend/api/migrations/0003_remove_concert_artist_remove_concert_venue_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0002_emailverificationcode'),
+        ('api', '0002_emailverificationtoken'),
     ]
 
     operations = [
