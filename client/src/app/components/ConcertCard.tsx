526f56d2e5251544f7d2019666b4b8c39a04e854
Oditha <oditha14@gmail.com>
2025-04-01 08:19:50 -0400

    spinner on concert load + fixed concert card format

--- client/src/app/components/ConcertCard.tsx
+++ client/src/app/components/ConcertCard.tsx
@@ -20,11 +20,11 @@ interface ConcertCardProps {
 
 const formatDate = (dateString: string) => {
   const date = new Date(dateString);
-  return date.toLocaleDateString('en-US', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
+  return date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
   });
 };
 
@@ -34,7 +34,7 @@ const ConcertCard: React.FC<ConcertCardProps> = ({
   date,
   imageUrl,
   info,
-  venue
+  venue,
 }) => {
   const [open, setOpen] = useState(false);
   const [isFavorite, setIsFavorite] = useState(false);
@@ -125,10 +125,10 @@ const ConcertCard: React.FC<ConcertCardProps> = ({
   return (
     <>
       <Card
-        sx={{ 
+        sx={{
           backgroundColor: "#1A1A1A",
-          borderRadius: '1.5rem', // This matches Tailwind's rounded-3xl
-          overflow: 'hidden'
+          borderRadius: "1.5rem", // This matches Tailwind's rounded-3xl
+          overflow: "hidden",
         }}
         className="w-80 h-60 cursor-pointer shadow-lg transition-all duration-300 ease-in-out hover:scale-105 drop-shadow-[0_0_15px_rgba(76,29,149,0.9)]"
         onClick={() => {
@@ -144,13 +144,17 @@ const ConcertCard: React.FC<ConcertCardProps> = ({
           className="rounded-t-3xl"
         />
         <CardContent>
-          <div ref={containerRef} className="overflow-hidden whitespace-nowrap rounded-3xl">
+          <div
+            ref={containerRef}
+            className="overflow-hidden whitespace-nowrap rounded-3xl"
+          >
             <Typography
               ref={titleRef}
               fontSize={16}
               variant="h6"
-              className={`font-semibold text-white inline-block ${isOverflowing ? "hover:animate-marquee" : ""
-                }`}
+              className={`font-semibold text-white inline-block ${
+                isOverflowing ? "hover:animate-marquee" : ""
+              }`}
             >
               {title}
             </Typography>
@@ -175,25 +179,33 @@ const ConcertCard: React.FC<ConcertCardProps> = ({
             alt={title}
             className="rounded-l-3xl object-cover"
           />
-          <div className="p-8 shadow-lg w-full relative">
-            <Typography variant="h4" fontWeight={800} className="font-semibold text-white">
+          <div className="m-8 shadow-lg w-full overflow-y-scroll">
+            <Typography
+              variant="h4"
+              fontWeight={800}
+              className="font-semibold text-white m-2 break-words"
+            >
               {title}
             </Typography>
             <Typography variant="h5" className="text-violet-600">
               {formatDate(date)}
             </Typography>
-            <Typography marginTop={1}>
-              {info}
-            </Typography>
-            <Typography fontSize={18} fontWeight={1000} marginTop={1} className="text-yellow-600">
+            <Typography marginTop={1}>{info}</Typography>
+            <Typography
+              fontSize={18}
+              fontWeight={1000}
+              marginTop={1}
+              className="text-yellow-600"
+            >
               {venue}
             </Typography>
 
             <FaStar
               size={24}
               onClick={() => toggleFavorite(id)}
-              className={`absolute bottom-5 right-5 hover:cursor-pointer ${isFavorite ? "text-yellow-600" : "text-slate-400"
-                }`}
+              className={`absolute bottom-5 right-5 hover:cursor-pointer ${
+                isFavorite ? "text-yellow-600" : "text-slate-400"
+              }`}
             />
           </div>
         </Box>
